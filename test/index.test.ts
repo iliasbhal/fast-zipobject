3ab9d003fb3f4324e6135506f0d4bce92e8ccc33
ilias bhallil <ilias.bhal@gmail.com>
2021-04-01 06:39:24 -0400

    Test: add speed tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,6 @@
 import fastZipObject from '../src';
+import { performance } from 'perf_hooks';
+import * as _ from 'lodash';
 
 describe('fastZipObject', () => {
   it('should access attributes', () => {
@@ -27,4 +29,29 @@ describe('fastZipObject', () => {
     expect(zipped.prop1).toEqual('bye');
   });
 
+  it('should be faster than lodash', () => {
+    testSpeedImprovement(5,1.1);
+    testSpeedImprovement(10,2);
+    testSpeedImprovement(20,3);
+
+
+    function testSpeedImprovement(numberOfProps: number, expectedSpeedYield: number) {
+      const props = _.range(0, numberOfProps).map((num) => `prop${num}`);
+      const values = _.range(0, numberOfProps).map((num) => `values${num}`);
+  
+      const startLodash = performance.now();
+      for (let i = 0; i < 1000000; i++) {
+        _.zipObject(props, values);
+      }
+      const lodashSpeed = performance.now() - startLodash;
+  
+  
+      const startFastZip = performance.now();
+      for (let i = 0; i < 1000000; i++) {
+        fastZipObject(props, values);
+      }
+      const fastZipSpeed = performance.now() - startFastZip;
+      expect(fastZipSpeed * expectedSpeedYield).toBeLessThan(lodashSpeed);
+    }
+  })
 });
