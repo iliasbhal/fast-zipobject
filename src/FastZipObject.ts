c8aeba4a846d99aaa24e18ce23490ca7c343c1c0
ilias bhallil <ilias.bhal@gmail.com>
2021-04-03 01:41:27 -0400

    Refac: move FastZipObject to its own file

---
+++ src/FastZipObject.ts
@@ -0,0 +1,26 @@
+export class FastZipObject {
+  static zipIndexesByProps = new Map<string[], Record<string, number>>();
+  static getZipIndexOfProp = (headers: string[]) => {
+    let memoizedIndexes = FastZipObject.zipIndexesByProps.get(headers);
+    if (memoizedIndexes) {
+      return memoizedIndexes;
+    }
+
+    memoizedIndexes = headers.reduce((acc, header, i) => {
+      acc[header] = i;
+      return acc;
+    }, {} as Record<string, number>);
+    FastZipObject.zipIndexesByProps.set(headers, memoizedIndexes);
+    return memoizedIndexes;
+  };
+
+  static createZipObject(props: string[], values: string[]) {
+    const obj = new Proxy({} as Record<string, unknown>, {
+      get(target, prop: string) {
+        return target[prop] 
+          || values[FastZipObject.getZipIndexOfProp(props)[prop]]
+      }
+    });
+    return obj;
+  }
+}
